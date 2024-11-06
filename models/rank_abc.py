205148da3a2a7bdde5aaaec97d23625286de1abf
qnhn2 <nhatquan234@gmail.com>
2024-11-05 23:46:29 -0500

    update

--- models/rank_abc.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class RankInterface(ABC):
-    def __init__(self, data):
-        self.data = data  
-    @abstractmethod
-    def rank_data(self):
-        pass
-
